897f21faad5f7462b4c56cc676ca7cd27bc0ae81
GabrielGorog <gorog1338@yahoo.com>
2019-11-20 20:30:09 +0200

    added modulo example

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -79,6 +79,9 @@ public class App
 //        Car car4 = null;
 //        System.out.println(car4.name);
 
+//        System.out.println("Modulo example:");
+//        System.out.println(4 % 2 == 0);
+//        System.out.println(4 % 3);
 
     }
 }
