f4e1f640a4bd4aa3effa0b4982abe7f730ec65b1
GabrielGorog <gorog1338@yahoo.com>
2019-11-27 21:19:28 +0200

    studied encapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -21,12 +21,12 @@ public class App
         carEngine.capacity = 1.5;
 
         Car carReference = new Car(carEngine);
-        carReference.name = "Dacia";
-        carReference.color = "red";
-        carReference.mileage = 9.8;
-        carReference.fuelLevel = 60;
-        carReference.maxSpeed = 200;
-        carReference.running = false;
+        carReference.setName("Dacia");
+        carReference.setColor("red");
+        carReference.setMileage(9.8);
+        carReference.setFuelLevel(60);
+        carReference.setMaxSpeed(200);
+        carReference.setRunning(false);
         carReference.doorCount = 2;
 
 
@@ -41,7 +41,7 @@ public class App
         Mechanic mechanic = new Mechanic();
         mechanic.repairVehicle(carReference);
 
-        System.out.println("Total traveled distance after repair: " +carReference.traveledDistance);
+        System.out.println("Total traveled distance after repair: " +carReference.getTraveledDistance());
 
         Engine car2Engine = new Engine();
         car2Engine.manufacturer = "Bavaria Motors";
@@ -49,38 +49,38 @@ public class App
 
 
         Car car2 = new Car(car2Engine);
-        car2.name = "BMW";
-        car2.mileage = 14;
-        car2.color = null;
+        car2.setName("BMW");
+        car2.setMileage(14);
+        car2.setColor (null);
 
 
         // concatenation
         System.out.println(carReference);
-        System.out.println("First car name: " + carReference.name);
-        System.out.println(carReference.color);
-        System.out.println(carReference.mileage);
-        System.out.println(carReference.fuelLevel);
-        System.out.println(carReference.maxSpeed);
-        System.out.println(carReference.running);
+        System.out.println("First car name: " + carReference.getName());
+        System.out.println(carReference.getColor());
+        System.out.println(carReference.getMileage());
+        System.out.println(carReference.getFuelLevel());
+        System.out.println(carReference.getMaxSpeed());
+        System.out.println(carReference.isRunning());
         System.out.println(carReference.doorCount);
-        System.out.println(carReference.traveledDistance);
+        System.out.println(carReference.getTraveledDistance());
 
-        System.out.println("Second car name: " + car2.name);
-        System.out.println(car2.color);
-        System.out.println(car2.mileage);
-        System.out.println(car2.fuelLevel);
-        System.out.println(car2.maxSpeed);
-        System.out.println(car2.running);
+        System.out.println("Second car name: " + car2.getName());
+        System.out.println(car2.getColor());
+        System.out.println(car2.getMileage());
+        System.out.println(car2.getFuelLevel());
+        System.out.println(car2.getMaxSpeed());
+        System.out.println(car2.isRunning());
         System.out.println(car2.doorCount);
-        System.out.println(car2.traveledDistance);
+        System.out.println(car2.getTraveledDistance());
 
-        car2.name = "VW";
-        System.out.println(car2.name);
+        car2.getName();
+        System.out.println(car2.getName());
 
         Car car3 = car2;
-        car3.name = "Audi";
-        System.out.println("car2 name : " + car2.name);
-        System.out.println("car3 name: " + car3.name);
+        car3.getName();
+        System.out.println("car2 name : " + car2.getName());
+        System.out.println("car3 name: " + car3.getName());
 
         // example for NullPointerException
 //        Car car4 = null;
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -7,13 +7,13 @@ public class Vehicle {
 
 
     // instance variables
-    String name;
-    String color;
-    double mileage;
-    double maxSpeed;
-    double fuelLevel;
-    double traveledDistance;
-    boolean running;
+    private String name;
+    private String color;
+    private double mileage;
+    private double maxSpeed;
+    private double fuelLevel;
+    private double traveledDistance;
+    private boolean running;
 
     public Vehicle() {
         totalCount++;
@@ -66,6 +66,62 @@ public class Vehicle {
         System.out.println("Remaining fuel level: " + fuelLevel);
 
         return distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public double getTraveledDistance() {
+        return traveledDistance;
+    }
+
+    public void setTraveledDistance(double traveledDistance) {
+        this.traveledDistance = traveledDistance;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
     }
 
     // Date creationDate = new Date(2019. 30, 11);
