0abb9a16170019abebbbf998138ad864d43b1f41
GabrielGorog <gorog1338@yahoo.com>
2019-12-11 19:23:09 +0200

    studied lists and enhanced for

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,10 +1,14 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
 
     private Track[] tracks = new Track[3];
+    private List<Vehicle> competitors = new ArrayList<>();
 
 
     public void start() {
@@ -13,14 +17,36 @@ public class Game {
         initializeTracks();
         displayTracks();
 
-//        int playerCount = getPlayerCountFromUser();
-//        System.out.println("Number of players:" + playerCount);
-//
-//        String vehicleName = getVehicleNameFromUser();
-//        System.out.println("Name from user: " + vehicleName);
+        initializeCompetitors();
 
+        // enhanced for
+        for (Vehicle vehicle : competitors) {
+            System.out.println("It's " + vehicle.getName() + "'s turn.");
+        }
 
     }
+
+    private void initializeCompetitors() {
+
+        int playerCount = getPlayerCountFromUser();
+        System.out.println("Number of players:" + playerCount);
+
+        for (int i = 1; i <= playerCount; i++) {
+            System.out.println("Adding player " + i);
+
+            Vehicle vehicle = new Vehicle();
+            vehicle.setName(getVehicleNameFromUser());
+            vehicle.setFuelLevel(80);
+            vehicle.setMileage(ThreadLocalRandom.current().nextDouble(6, 15));
+            vehicle.setMaxSpeed(300);
+
+            System.out.println("Vehicle for player " + i + ": " + vehicle.getName() + " - Mileage: " + vehicle.getMileage());
+
+            competitors.add(vehicle);
+        }
+    }
+
+
 
     private void initializeTracks() {
         Track track1 = new Track();
@@ -47,18 +73,18 @@ public class Game {
         }
     }
 
-//
-//
-//    private String getVehicleNameFromUser() {
-//        System.out.println("Please enter vehicle name");
-//        Scanner scanner = new Scanner(System.in);
-//        return scanner.nextLine();
-//
-//    }
-//
-//    private int getPlayerCountFromUser() {
-//        System.out.println("Please enter player count");
-//        Scanner scanner = new Scanner(System.in);
-//        return scanner.nextInt();
-//    }
+
+
+       private String getVehicleNameFromUser() {
+        System.out.println("Please enter vehicle name");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+
+    }
+
+    private int getPlayerCountFromUser() {
+        System.out.println("Please enter player count");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 }
