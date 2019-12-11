aca0392cad1ca430ddda116d92fe276c17dce2e2
GabrielGorog <gorog1338@yahoo.com>
2019-12-11 21:03:43 +0200

    studied recursion

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -9,8 +9,7 @@ import java.sql.SQLOutput;
 public class App 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
         System.out.println( "Welcome to the Racing Game!" );
 
         Game game = new Game();
